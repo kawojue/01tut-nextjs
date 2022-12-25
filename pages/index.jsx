0659182522d64313baa608cd969cdc36b6547bd3
Raheem Kawojue <kawojue08@gmail.com>
2022-12-25 12:24:26 +0100

    [fetch] json data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,31 @@
-const Home = () => {
+import Head from 'next/head'
+
+export default function Home({ articles }) {
+  console.log(articles)
   return (
     <>
-      <p>Welcome!</p>
+      <Head>
+        <title>WebDev News</title>
+        <meta name="keywords"
+          content="web development, programming" />
+      </Head>
+      <div>
+        {articles.map(article => {
+          <h3 className="text-black">
+            {article.title}
+          </h3>
+        })}
+      </div>
     </>
   )
 }
 
-export default Home
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+  return {
+    props: {
+      articles
+    }
+  }
+}
