ea94d1eb529f5868b3d5734a167100111c9b196a
Raheem Kawojue <kawojue08@gmail.com>
2022-12-26 01:14:05 +0100

    [edit]

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,10 +2,13 @@ import headerStyles from '../styles/Header.module.css'
 
 const Header = () => {
     return (
-        <header className={headerStyles.title}>
-            <h1>
+        <header className={headerStyles.header}>
+            <h1 className={headerStyles.title}>
                 <span>WebDev</span>News
             </h1>
+            <p className={headerStyles.description}>
+                Keep up to date with latest Web Dev News.
+            </p>
         </header>
     )
 }
