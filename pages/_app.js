79c10bcc0197ace22c767d1e8d2eaca032eb5ca3
Raheem Kawojue <kawojue08@gmail.com>
2022-12-27 10:22:26 +0100

    [add] data provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import { DataProvider } from '../components/Context'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <DataProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </DataProvider>
   )
 }
 
