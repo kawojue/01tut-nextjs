50617bc2407313dc3ff09deec2bce1ee962cb869
Raheem Kawojue <kawojue08@gmail.com>
2022-12-28 18:41:15 +0100

    [add] staticProps & staticPaths

--- pages/article/[id]/index.jsx
+++ pages/article/[id]/index.jsx
@@ -1,32 +1,55 @@
-import { useRouter } from 'next/router'
-import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { useContext } from 'react'
+import Context from '../../../components/Context'
+import ArticleStyles from '../../../styles/Article.module.css'
 
-const index = () => {
-    const router = useRouter()
-    const { id } = router.query
-    const [article, setArticle] = useState({})
+const Article = ({ article }) => {
+    const { title, body } = article
+    const { err, isLoading } = useContext(Context)
 
-    const getArticle = async (id) => {
-        try {
-            const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-            const data = await res.json()
-            setArticle(data)
-        } catch {
-
-        }
+    if (err) {
+        return (
+            <>
+                <h1>{err}</h1>
+            </>
+        )
     }
 
-    useEffect(() => {
-        (async () => await getArticle(id))()
-    }, [])
-
-    console.log(article)
+    if (isLoading) {
+        return (
+            <>
+                <h1>Loading...</h1>
+            </>
+        )
+    }
 
     return (
-        <main>
+        <main className={ArticleStyles.main}>
+            <h3>{title}</h3>
+            <p>{body}</p>
 
+            <Link href="/">go back</Link>
         </main>
     )
 }
 
-export default index
+
+export const getStaticProps = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const article = await res.json()
+    return {
+        props: {
+            article
+        }
+    }
+}
+
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const articles = await res.json()
+    const paths = articles.map(article => `/article/${article.id}`)
+
+    return { paths, fallback: false }
+}
+
+export default Article
