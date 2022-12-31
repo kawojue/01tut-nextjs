8f7062c83c55a8b662f295b5960625ced4686bec
Raheem Kawojue <kawojue08@gmail.com>
2022-12-31 22:04:29 +0100

    [fetch] data with getStaticProps

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,8 @@
+import axios from 'axios'
 import Head from 'next/head'
 import ArticleList from '../components/ArticleList'
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <>
       <Head>
@@ -9,9 +10,17 @@ export default function Home() {
         <meta name="keywords"
           content="web development, programming" />
       </Head>
-      <ArticleList />
+      <ArticleList articles={articles} />
     </>
   )
 }
 
-// getStaticProps
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=200`)
+  const articles = await res.json()
+  return {
+    props: {
+      articles
+    }
+  }
+}
