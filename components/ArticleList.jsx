9bacca2d376b7205b6cc2b42761fa08b4155cd53
Raheem Kawojue <kawojue08@gmail.com>
2022-12-31 21:58:51 +0100

    [edit] refractored

--- components/ArticleList.jsx
+++ components/ArticleList.jsx
@@ -1,27 +1,7 @@
-import Context from './Context'
-import { useContext } from 'react'
 import ArticleItem from './ArticleItem'
 import ArticleStyles from '../styles/Article.module.css'
 
-const ArticleList = () => {
-    const { err, isLoading, articles } = useContext(Context)
-
-    if (err) {
-        return (
-            <>
-                <h1>{err}</h1>
-            </>
-        )
-    }
-
-    if (isLoading) {
-        return (
-            <>
-                <h1>Loading...</h1>
-            </>
-        )
-    }
-
+const ArticleList = ({ articles }) => {
     return (
         <article className={`lg:grid-cols-3 md:grid-cols-2 ${ArticleStyles.list}`}>
             {articles.map(article => (
