f176f35c6c9ff326af43e157208b7f6c1f234b31
Raheem Kawojue <kawojue08@gmail.com>
2023-01-02 01:08:26 +0100

    refractored

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
-import { DataProvider } from '../components/Context'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <DataProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </DataProvider>
   )
 }
 
