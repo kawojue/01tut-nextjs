f41968c390b646935411db2a781dec17f367338b
Raheem Kawojue <kawojue08@gmail.com>
2023-01-19 00:31:46 +0100

    [fix] keywords & description

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -4,9 +4,10 @@ const Meta = ({ keywords, title, description }) => {
     return (
         <Head>
             <title>{title}</title>
-            <meta charset="UTF-8" />
+            <meta charSet="UTF-8" />
             <link rel="icon" href="/favicon.ico" />
-            <meta name="keywords" content={description} />
+            <meta name="keywords" content={keywords} />
+            <meta name="description" content={description} />
             <meta http-equiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </Head>
