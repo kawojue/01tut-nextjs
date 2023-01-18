1662f3fe29b7716969ba10bf2d090c760f8d2ea5
Raheem Kawojue <kawojue08@gmail.com>
2023-01-19 00:32:45 +0100

    [edit]

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000': 'https://vercel.com/alwaysappear'
+export const server = dev ? 'http://localhost:3000': ''
